d9b62fdc02a326398e72e53d2a591e1032167415
Nitin Chaurasia <gs.nitin@gmail.com>
2020-01-26 00:51:24 -0500

    Added initial MQ Project

---
+++ src/main/java/com/configurer/rabbitMQconfigurer/RabbitMqConfigurerApplication.java
@@ -0,0 +1,13 @@
+package com.configurer.rabbitMQconfigurer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RabbitMqConfigurerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RabbitMqConfigurerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/configurer/rabbitMQconfigurer/RabbitMqConfigurerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.configurer.rabbitMQconfigurer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RabbitMqConfigurerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
